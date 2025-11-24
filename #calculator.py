a0fc5ec8c406a29c75f36c3f0619ee06f61ad9e4
ansh25306 <ansh.25bai10334@vitbhopal.ac.in>
2025-11-24 09:55:18 +0530

    Add files via upload

---
+++ #calculator.py
@@ -0,0 +1,27 @@
+#calculator
+def sum(a,b):
+    sum=a+b
+    print('sum=',sum)
+
+def subtraction(a,b):
+    subtraction=a-b
+    print('subtraction=',subtraction)
+
+def product(a,b):
+    product=a*b
+    print('product=',product)
+
+def division(a,b):
+    division=a/b
+    print('division=',division)
+
+def exponential(a,b):
+    exponential=a**b
+    print('exponential=',exponential)
+
+
+sum(33,67)
+product(25,20)
+subtraction(99,199)
+division(9,2)
+exponential(25,2)
